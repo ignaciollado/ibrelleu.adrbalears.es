899d6869bc1f4cba1af023cdaa0917f91200062b
ignaciollado <86431188+ignaciollado@users.noreply.github.com>
2024-11-28 13:03:46 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'adr-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'ibrelleu.adrbalears.es';
+}
