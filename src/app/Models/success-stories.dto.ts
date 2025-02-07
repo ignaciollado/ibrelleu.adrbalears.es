8ab01342783d51c147f8c4c4d0d50e6828d73ad7
JLDJadr <jose.luis@adrbalears.es>
2025-02-07 10:11:09 +0100

    Traducción listado casos de éxito. Cambiado enrutado, quitado col código

--- src/app/Models/success-stories.dto.ts
+++ src/app/Models/success-stories.dto.ts
@@ -49,87 +49,81 @@ export class SuccessStoriesDTO {
 
 export const successStoriesColumns = [
   {
-    key: 'id',
+    key: 'companyName',
     type: 'url',
-    label: 'Código',
-    /* label_ca: 'Codi', */
+    label: 'Nombre',
+    /*  label_ca: 'Nom', */
   },
   {
     key: 'delegation',
     type: 'text',
     label: 'Delegación',
-   /*  label_ca: 'Delegació', */
+    /*  label_ca: 'Delegació', */
   },
   {
-    key: 'companyName',
-    type: 'readOnly',
-    label: 'Nombre',
-   /*  label_ca: 'Nom', */
-  },
-  {
-    key: 'proces',
+    key: 'process',
     type: 'readOnly',
     label: 'Proceso',
- /*    label_ca: 'Procés', */
+    /*    label_ca: 'Procés', */
   },
   {
     key: 'grantorProject',
     type: 'readOnly',
     label: 'Proyecto cedente',
-   /*  label_ca: 'Projecte cedent', */
+    /*  label_ca: 'Projecte cedent', */
   },
   {
     key: 'reInterpriseProject',
     type: 'readOnly',
     label: 'Proyecto IbRelleu',
-/*     label_ca: 'Projecte IbRelleu', */
+    /*     label_ca: 'Projecte IbRelleu', */
   },
   {
     key: 'mainSector',
     type: 'readOnly',
     label: 'Sector principal',
-/*     label_ca: 'Sector principal', */
+    /*     label_ca: 'Sector principal', */
   },
   {
     key: 'activityType',
     type: 'readOnly',
     label: 'Actividad',
-/*     label_ca: 'Activitat', */
+    /*     label_ca: 'Activitat', */
   },
   {
     key: 'transferPrice',
     type: 'money',
     label: 'Precio de cesión',
-/*     label_ca: 'Preu de cesió', */
+    /*     label_ca: 'Preu de cesió', */
   },
   {
     key: 'numWorkersToBehired',
     type: 'number',
     label: 'Núm. de trabajadores para contratar',
-/*     label_ca: 'Núm. de treballadors per contractar', */
+    /*     label_ca: 'Núm. de treballadors per contractar', */
   },
   {
     key: 'numWorkersSaved',
     type: 'number',
     label: 'Núm. de trabajadores salvados',
-/*     label_ca: 'Núm. de treballadores salvats', */
+    /*     label_ca: 'Núm. de treballadores salvats', */
   },
   {
     key: 'numEntrepreneursWhoWillWork',
     type: 'number',
     label: 'Núm. de trabajadores emprendedores que trabajaran',
-/*     label_ca: 'Núm. de treballadors emprenedors que treballaran', */
+    /*     label_ca: 'Núm. de treballadors emprenedors que treballaran', */
   },
   {
     key: 'numPeopleWorkingAtIbRelleu',
     type: 'number',
     label: 'Núm. total de personas trabajadoras',
-/*     label_ca: 'Núm. total de persones treballadores', */
+    /*     label_ca: 'Núm. total de persones treballadores', */
   },
   {
     key: 'creationDate',
     type: 'text',
     label: 'Fecha de creación',
-/*     label_ca: 'Data de creació', */
-  }
+    /*     label_ca: 'Data de creació', */
+  },
 ];
