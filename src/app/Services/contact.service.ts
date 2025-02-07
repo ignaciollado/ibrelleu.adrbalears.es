07d028c3c4806e55534a4093102887fe863a015f
ignaciollado <86431188+ignaciollado@users.noreply.github.com>
2025-02-07 07:21:28 +0100

    he conectado contact.service al servidor data.ibrelleu.es

--- src/app/Services/contact.service.ts
+++ src/app/Services/contact.service.ts
@@ -1,31 +1,46 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'text/plain', /* la única forma de evitar errores de CORS ha sido añadiendo esta cabecera */
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
   private apiUrl = '../../assets/phpAPI/';
-
+  private apiData = 'https://data.ibrelleu.es/public/index.php';
   constructor(private http: HttpClient) { }
 
-  getContacts(): Observable<any> {
+/*   getContacts(): Observable<any> {
     return this.http.get(`${this.apiUrl}/contactList.php`).pipe(
       catchError(this.handleError));
-  }
+  } */
+
+    getContacts(): Observable<any> {
+      return this.http.get(`${this.apiData}/api/contacts/`, httpOptions).pipe(
+        catchError(this.handleError));
+    }
 
   getContact(id: number): Observable<any> {
     return this.http.get(`${this.apiUrl}/contactGetOne.php?id=${id}`).pipe(
       catchError(this.handleError));
   }
 
-  getContactByDNI(dni: string): Observable<any> {
+/*   getContactByDNI(dni: string): Observable<any> {
     return this.http.get(`${this.apiUrl}/contactGetOneByDNI.php?dni=${dni}`).pipe(
       catchError(this.handleError));
+  } */
+  
+  getContactByDNI(dni: string): Observable<any> {
+    return this.http.get(`${this.apiData}/contactGetOneByDNI?dni=${dni}`).pipe(
+      catchError(this.handleError));
   }
-
 
   createContact(contact: any): Observable<any> {
     return this.http.post(`${this.apiUrl}/contactInsert.php`, contact).pipe(
