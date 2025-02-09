110e8902458a20e577973612e1e5681c640484a1
ignacio.llado@idi.es <ignacio.llado@idi.es>
2025-02-09 18:07:16 +0100

    con proxy funciona en desarrollo (localhost:4200) pero no en producción

--- src/app/Services/document.service.ts
+++ src/app/Services/document.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpEvent, HttpEventType } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://data.ibrelleu.es/public/index.php/api/'  // Asegúrate de que las solicitudes apunten a '/api'
+  };
+  
