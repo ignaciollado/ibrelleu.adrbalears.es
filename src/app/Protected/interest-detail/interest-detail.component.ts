9c77f755ab6015f2384d12bc1952cdf645a5620f
JLDJadr <jose.luis@adrbalears.es>
2025-02-14 08:22:28 +0100

    copy-paste base de formulario

--- src/app/Protected/interest-detail/interest-detail.component.ts
+++ src/app/Protected/interest-detail/interest-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { MatTabChangeEvent } from '@angular/material/tabs';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'adr-interest-detail',
@@ -6,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrl: './interest-detail.component.scss'
 })
 export class InterestDetailComponent {
+  selectedIndex: number;
+  interestForm: FormGroup
+  id: string = this.route.snapshot.paramMap.get('id')
+
+  constructor(private route: ActivatedRoute) {
+    this.interestForm = new FormGroup({})
+  }
+
+
+  canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.interestForm.dirty) {
+      return confirm('You have unsaved changes. Do you really want to leave?');
+    }
+    return true;
+  }
+
+  onSubmit() {
+    console.log(this.interestForm.value);
+    // Aquí puedes llamar a tu servicio para guardar los datos en MariaDB
+  }
+
+  onTabChange(event: MatTabChangeEvent) {
+    sessionStorage.setItem('currentInterestTab', event.index.toString());
+  }
 
 }
