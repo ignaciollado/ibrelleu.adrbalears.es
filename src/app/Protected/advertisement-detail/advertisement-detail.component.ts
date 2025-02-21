ae8ed2d714ec51cf768b6fcbfe536e97e9d1df24
JLDJadr <jose.luis@adrbalears.es>
2025-02-21 10:24:51 +0100

    Printado de datos en formulario anuncio *CON ID*

--- src/app/Protected/advertisement-detail/advertisement-detail.component.ts
+++ src/app/Protected/advertisement-detail/advertisement-detail.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { DataService } from '../../Services/data.service';
 import { GrantorProjectsDTO } from '../../Models/grantorProject.dto';
+import { AdvertisementService } from '../../Services/advertisement.service';
+import { AdvertisementDTO } from '../../Models/advertisement.dto';
 
 @Component({
   selector: 'adr-advertisement-detail',
@@ -18,9 +20,15 @@ export class AdvertisementDetailComponent {
 
   grantorProjectList: GrantorProjectsDTO[] = []
 
-  constructor(private route: ActivatedRoute, private dataService: DataService) {
+  constructor(private route: ActivatedRoute, private dataService: DataService, private adService: AdvertisementService) {
     this.adForm = new FormGroup({
-
+      grantorProject: new FormControl(''),
+      publicinmarketplace: new FormControl(''),
+      title: new FormControl(''),
+      advertisementstate: new FormControl(''),
+      description: new FormControl(''),
+      language: new FormControl(''),
+      publicinweb: new FormControl(''),
     })
 
     this.loadAllGrantorProjects()
@@ -28,6 +36,11 @@ export class AdvertisementDetailComponent {
 
   ngOnInit() {
     this.id = this.route.snapshot.paramMap.get('id');
+
+    this.adService.getAllAdvertisements().subscribe((ads: AdvertisementDTO[]) => {
+      let targetAd: AdvertisementDTO = ads.find(ad => ad.id.toString() === this.id)
+      this.loadAdFormInfo(targetAd)
+    })
   }
 
   canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
@@ -44,6 +57,18 @@ export class AdvertisementDetailComponent {
   loadAllGrantorProjects() {
     this.dataService.getAllGrantorProjects().subscribe((grantorProjects: GrantorProjectsDTO[]) => {
       this.grantorProjectList = grantorProjects
+    })
+  }
+
+  loadAdFormInfo(ad: AdvertisementDTO) {
+    this.adForm.patchValue({
+      grantorProject: ad.grantorprojectid,
+      publicinmarketplace: ad.publicinmarketplace,
+      title: ad.title,
+      advertisementstate: ad.advertisementstate,
+      description: ad.description,
+      language: ad.language,
+      publicinweb: ad.publicinweb
     })
   }
 }
