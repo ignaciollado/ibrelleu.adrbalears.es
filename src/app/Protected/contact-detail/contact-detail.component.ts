d409e7ddc6464bcd86163617b10434aaeb1f1754
JLDJadr <jose.luis@adrbalears.es>
2025-02-25 14:19:37 +0100

    Revierto botón de Crear contacto

--- src/app/Protected/contact-detail/contact-detail.component.ts
+++ src/app/Protected/contact-detail/contact-detail.component.ts
@@ -19,7 +19,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrl: './contact-detail.component.scss',
 })
 export class ContactDetailComponent implements CanComponentDeactivate {
-  id: string | null = this.route.snapshot.paramMap.get('id');
+  id: string = this.route.snapshot.paramMap.get('id');
   selectedIndex: number;
 
   expandedIndex = 0;
@@ -109,7 +109,7 @@ export class ContactDetailComponent implements CanComponentDeactivate {
     this.getAllZipCodes();
     this.getLevelOfEducationList();
     this.loadConsultantAndDelegationInfo();
-    if (this.id) { this.getContactById(+this.id); }
+    this.getContactById(+this.id)
   }
 
   ngOnInit() {
